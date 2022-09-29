a96384f003a762c205dcfcea181e230a04206f81
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-09-29 22:39:58 +0900

    [TF] Practice2T18L3-16184:学習分つづき:更新分

--- TechFUL/Practice/C2/T18/L3/16184/main1-0.cpp
+++ TechFUL/Practice/C2/T18/L3/16184/main1-0.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 using ll = long long;
 
+const ll LINF = (ll)(1e18);
+
 int main() {
     int t0;
     cin >> t0;
@@ -17,37 +19,76 @@ int main() {
             cout << 0 << endl;
             continue;
         }
-        vector<vector<vector<vector<ll>>>> dp(62, vector<vector<vector<ll>>>(2, vector<vector<ll>>(ci-1, vector<ll>(2))));
-        int res = 1;
+        vector<vector<vector<vector<ll>>>> dp(62, vector<vector<vector<ll>>>(2, vector<vector<ll>>(ci-1, vector<ll>(2, -LINF))));
+        int bt = 63 - __builtin_clzll(ai), res = 1;
         dp[0][0][0][0] = dp[0][0][0][1] = 0;
         dp[0][1][0][1] = 1;
-        dp[0][1][0][0] = (ai & 1LL);
+        dp[0][1][0][0] = (ai%2==1) ? 1 : -LINF;
+        bool fin = false;
         for (int i=1; i<62; ++i) {
-            bool ok = ((1LL<<(i+1)) <= ai);
-            for (int k=1; k<ci-1; ++k) {
-                if ((1LL<<(i+1)) <= ai) {
-                    dp[i][0][k][1] = max(dp[i][0][k][1], dp[i-1][0][k-1][1]);
-                    dp[i][1][k][1] = max(dp[i][1][k][1], dp[i-1][1][k-1][1]+(1LL<<i));
+            fin = true;
+            for (int j=0; j<ci-1; ++j) {
+                if (ai&(1LL<<i)) {
+                    if (j-1>=0 && dp[i-1][0][j-1][1]!=-LINF && dp[i-1][0][j-1][1]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][0][j][0] = max(dp[i][0][j][0], dp[i-1][0][j-1][1]);
+                        fin = false;
+                    }
+                    if (dp[i-1][1][j][1]!=-LINF && dp[i-1][1][j][1]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][0][0][0] = max(dp[i][0][0][0], dp[i-1][1][j][1]);
+                        fin = false;
+                    }
+                    if (j-1>=0 && dp[i-1][1][j-1][0]!=-LINF && dp[i-1][1][j-1][0]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                        dp[i][1][j][0] = max(dp[i][1][j][0], dp[i-1][1][j-1][0]+(1LL<<i));
+                        fin = false;
+                    }
+                    if (dp[i-1][0][j][0]!=-LINF && dp[i-1][0][j][0]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                        dp[i][1][0][0] = max(dp[i][1][0][0], dp[i-1][0][j][0]+(1LL<<i));
+                        fin = false;
+                    }
                 }
-                dp[i][0][k][0] = max(dp[i][0][k][0], dp[i-1][0][k-1][0]);
-                if (dp[i][0][k][0]+bi >= (1LL<<(i+1))-1) ok = true;
-                dp[i][1][k][0] = max(dp[i][1][k][0], dp[i-1][1][k-1][0]);
-                if (ai&(1LL<<i)) dp[i][1][k][0] = max(dp[i][1][k][0], dp[i-1][1][k-1][0]+(1LL<<i));
-                if (dp[i][1][k][0]+bi >= (1LL<<(i+1))-1) ok = true;
-            }
-            for (int k=0; k<ci-1; ++k) {
-                if ((1LL<<(i+1)) <= ai) {
-                    dp[i][0][0][1] = max(dp[i][0][0][1], dp[i-1][1][k][1]);
-                    dp[i][1][0][1] = max(dp[i][1][0][1], dp[i-1][0][k][1]);
+                else {
+                    if (j-1>=0 && dp[i-1][0][j-1][0]!=-LINF && dp[i-1][0][j-1][0]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][0][j][0] = max(dp[i][0][j][0], dp[i-1][0][j-1][0]);
+                        fin = false;
+                    }
+                    if (dp[i-1][1][j][0]!=-LINF && dp[i-1][1][j][0]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][0][0][0] = max(dp[i][0][0][0], dp[i-1][1][j][0]);
+                        fin = false;
+                    }
                 }
-                dp[i][0][0][0] = max(dp[i][0][0][0], dp[i][1][k][0]);
-                if (dp[i][0][0][0]+bi >= (1LL<<(i+1))-1) ok = true;
-                dp[i][1][0][0] = max(dp[i][1][0][0], dp[i][0][k][0]);
-                if (ai&(1LL<<i)) dp[i][1][0][0] = max(dp[i][1][0][0], dp[i][0][k][0]+(1LL<<i));
-                if (dp[i][1][0][0]+bi >= (1LL<<(i+1))-1) ok = true;
+                if (i < bt) {
+                    for (int i2=0; i2<2; ++i2) if (j-1>=0 && dp[i-1][i2][j-1][1]!=-LINF && dp[i-1][i2][j-1][1]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][i2][j][1] = max(dp[i][i2][j][1], dp[i-1][i2][j-1][1]);
+                        fin = false;
+                    }
+                    if (dp[i-1][1][j][1]!=-LINF && dp[i-1][1][j][1]+bi>=(1LL<<(i+1))-1) {
+                        dp[i][0][0][1] = max(dp[i][0][0][1], dp[i-1][1][j][1]);
+                        fin = false;
+                    }
+                    if (ai&(1LL<<i)) {
+                        if (j-1>=0 && dp[i-1][1][j-1][1]!=-LINF && dp[i-1][1][j-1][1]+(1LL<<i)<=ai && dp[i-1][1][j-1][1]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                            dp[i][1][j][1] = max(dp[i][1][j][1], dp[i-1][1][j-1][1]+(1LL<<i));
+                            fin = false;
+                        }
+                        if (dp[i-1][0][j][1]!=-LINF && dp[i-1][0][j][1]+(1LL<<i)<=ai && dp[i-1][0][j][1]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                            dp[i][1][0][1] = max(dp[i][1][0][1], dp[i-1][0][j][1]+(1LL<<i));
+                            fin = false;
+                        }
+                    }
+                    else {
+                        if (j-1>=0 && dp[i-1][1][j-1][1]!=-LINF && dp[i-1][1][j-1][1]+(1LL<<i)<=ai && dp[i-1][1][j-1][1]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                            dp[i][1][j][1] = max(dp[i][1][j][1], dp[i-1][1][j-1][1]+(1LL<<i));
+                            fin = false;
+                        }
+                        if (dp[i-1][0][j][1]!=-LINF && dp[i-1][0][j][1]+(1LL<<i)<=ai && dp[i-1][0][j][1]+(1LL<<i)+bi>=(1LL<<(i+1))-1) {
+                            dp[i][1][0][1] = max(dp[i][1][0][1], dp[i-1][0][j][1]+(1LL<<i));
+                            fin = false;
+                        }
+                    }
+                }
             }
-            if (ok) res = max(res, i+1);
-            else break;
+            if (fin) break;
+            res = max(res, i+1);
         }
         cout << res << endl;
     }
