807853dd97fd0ca2d7a783881a520cb7229ff6a8
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-04-18 23:52:27 +0900

    [AM] C19S06Q07:学習1回目

---
+++ アルゴ式/C19/S06/Q07/1/main0.cpp
@@ -0,0 +1,32 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n);
+    for (int i=0; i<n-1; ++i) {
+        int ai, bi;
+        cin >> ai >> bi;
+        g[ai].push_back(bi);
+        g[bi].push_back(ai);
+    }
+    vector<bool> checked(n);
+    int res = 0;
+    auto dfs = [&](auto& dfs, int vi=0, int pi=-1) -> void {
+        bool ok = true;
+        for (const int& ti : g[vi]) if (ti != pi) {
+            dfs(dfs, ti, vi);
+            if (checked[ti]) ok = false;
+        }
+        if (ok) {
+            checked[vi] = true;
+            ++res;
+        }
+    };
+    dfs(dfs);
+    cout << res << endl;
+	return 0;
+}
