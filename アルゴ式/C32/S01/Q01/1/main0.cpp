7968e152e8ec10cc1e0e662177c55316fdef2182
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-04-21 19:58:28 +0900

    [AM] C32S01-Q01:学習1回目

---
+++ アルゴ式/C32/S01/Q01/1/main0.cpp
@@ -0,0 +1,11 @@
+// 自力AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sa, ta, sb, tb;
+    cin >> sa >> ta >> sb >> tb;
+    cout << max(0, min(ta, tb) - max(sa, sb)) << endl;
+	return 0;
+}
